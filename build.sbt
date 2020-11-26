d25be3c7febf6388d20bebc5d951d5eb52484775
vadopolski <vadopolski@gmail.com>
2020-11-27 00:01:12 +0300

    Added new tasks

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ version := "0.1"
 
 scalaVersion := "2.12.10"
 
-val sparkVersion = "3.0.1"
+val sparkVersion = "2.4.7"
 val vegasVersion = "0.3.11"
 val postgresVersion = "42.2.2"
 
---
+++ src/main/scala/homework1/DataApiHomeWork.scala
@@ -0,0 +1,35 @@
+package homework1
+
+import org.apache.spark.sql.SparkSession
+
+object DataApiHomeWork extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Joins")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val driver = "org.postgresql.Driver"
+  val url = "jdbc:postgresql://localhost:5432/otus"
+  val user = "docker"
+  val password = "docker"
+
+
+
+  /**
+   * Описание источника:
+   * файл с зарплатами находится в src/main/resources/salary.csv
+   * выше указаны параметры подключения для базы данных с источником sql/db.sql
+   *
+   * Задание написать код, который будет делать следующее:
+   *
+   * 1. DataFrame построить витрину с 10-ю самыми оплачиваемыми должностями в компании. Результат в Parquet.
+   * 2. DataSet построить витрину с сотрудниками зарплата которых выше среднего. Результат в txt файл c пробелами.
+   * 3. RDD рассчитать среднюю зарплату у всех сотрудников за все все время. Результат записать в базу данных Postgres.
+   */
+
+
+
+
+}
+
